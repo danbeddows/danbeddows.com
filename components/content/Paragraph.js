d48d47d56dbcf8ed84c5dbd9ffa7d72034e926a8
Dan Beddows <dan@cloudmix.net>
2021-02-02 13:21:35 +0000

    changed body font to wotfard

--- components/content/Paragraph.js
+++ components/content/Paragraph.js
@@ -3,10 +3,10 @@ import styled from "styled-components";
 const StyledParagraph = styled.p`
   margin: 1rem 0;
   font-weight: 400;
-  font-size: 1.15rem;
+  font-size: 1.1rem;
   line-height: 1.6;
   letter-spacing: 0rem;
-  font-family: "silka", Roboto, Oxygen, Ubuntu, Droid Sans, Helvetica Neue,
+  font-family: "wotfard", Roboto, Oxygen, Ubuntu, Droid Sans, Helvetica Neue,
     sans-serif;
 
   @media screen and (min-width: 769px) {
--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,22 @@ import Springboard from "../components/global/Springboard";
 import "../styles/globals.scss";
 
 const GlobalStyles = createGlobalStyle`
-@font-face {
-  font-family: "silka";
-  src: url("/font/silka/silka-regular-webfont.woff2") format("woff2"),
-    url("/font/silka/silka-regular-webfont.woff") format("woff"),
-    url("/font/silka/silka-regular-webfont.ttf") format("truetype");
-  font-weight: normal;
-  font-style: normal;
-}
+	@font-face {
+		font-family: "silka";
+		src: url("/font/silka/regular.woff2") format("woff2"),
+			url("/font/silka/regular.woff") format("woff"),
+			url("/font/silka/regular.ttf") format("truetype");
+		font-weight: normal;
+		font-style: normal;
+	}
+
+	@font-face {
+		font-family: "wotfard";
+		src: url("/font/wotfard/regular.woff2") format("woff2"),
+			url("/font/wotfard/regular.ttf") format("truetype");
+		font-weight: normal;
+		font-style: normal;
+	}
 
   body {
 		padding: 0;
