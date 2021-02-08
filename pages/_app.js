b79fdd75ea7b6fa849eabcb571a21bcdb86d5b41
Dan Beddows <dan@cloudmix.net>
2021-02-08 10:03:39 +0000

    refactorered app.js style components to styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
 import Head from "next/head";
 import { useEffect } from "react";
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import Springboard from "../components/global/Springboard";
 import "../styles/globals.scss";
 
 const GlobalStyles = createGlobalStyle`
-
 	@font-face {
 		font-family: "wotfard";
 		src: url("/font/wotfard/regular.woff2") format("woff2"),
@@ -24,6 +23,47 @@ const GlobalStyles = createGlobalStyle`
 	* {
 		box-sizing: border-box;
 	}
+`;
+
+const PageContainer = styled.div`
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  @media screen and (min-width: 769px) {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+  }
+`;
+
+const PageSpringboard = styled.div`
+  @media screen and (max-width: 768px) {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+  }
+
+  @media screen and (min-width: 769px) {
+    min-width: 300px;
+    flex: 0 0 25vw;
+    background: #293462;
+  }
+`;
+
+const PageContent = styled.div`
+  @media screen and (max-width: 768px) {
+    padding-top: 12px;
+  }
+
+  @media screen and (min-width: 769px) {
+    flex: 1;
+  }
 `;
 
 function MyApp({ Component, pageProps }) {
@@ -52,14 +92,14 @@ function MyApp({ Component, pageProps }) {
         />
       </Head>
       <GlobalStyles />
-      <div className="page-container">
-        <div className="page-sidebar">
+      <PageContainer>
+        <PageSpringboard>
           <Springboard />
-        </div>
-        <div className="page-content">
+        </PageSpringboard>
+        <PageContent>
           <Component {...pageProps} />
-        </div>
-      </div>
+        </PageContent>
+      </PageContainer>
     </>
   );
 }
