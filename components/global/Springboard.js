31ca734b6d0e44a658b70bf52e15f36cf471df19
Dan Beddows <dan@cloudmix.net>
2021-03-28 10:18:33 +0100

    reduced flicker on page load

--- components/global/Springboard.js
+++ components/global/Springboard.js
@@ -8,11 +8,13 @@ import MobileButton from "./Springboard/MobileButton";
 const Container = styled(motion.aside)`
   width: 100%;
   height: 100%;
+  min-height: 100vh;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 
   @media screen and (max-width: 768px) {
     pointer-events: none;
+    min-height: 0;
   }
 `;
 
