c7aaabd28755b282b54e9c7324a453a313bcc5f2
Dan Beddows <dan@cloudmix.net>
2021-04-18 20:04:14 +0100

    updated favicon from emoji to image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -89,7 +89,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="UTF-8" />
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.png" />
         <style type="text/css">{fontAwesomeDom.css()}</style>
         <link
           href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700&display=swap"
