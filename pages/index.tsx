d6251b7172dfa82230a978828774a877e354534f
Dan Beddows <dan@cloudmix.net>
2021-07-21 16:41:44 +0100

    updated copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,11 @@ export default function Home() {
 
         <Paragraph>
           I’ve worn many hats over the past ten years. Working professionally as
-          a software developer, founding a couple of SaaS’s, and more recently
-          led the tech approach to a digital agency in Manchester; involving
-          working directly with clients to advise on their tech stack, develop
-          their product and act as a product owner, and then actually build and
-          ship.
+          a software developer, founding a couple of SaaS’s, and more recently I
+          led the tech approach at a digital agency in Manchester; which
+          involved working directly with clients to advise on their tech stack,
+          develop their product and act as a product owner, and then actually
+          build and ship.
         </Paragraph>
 
         <Paragraph>
