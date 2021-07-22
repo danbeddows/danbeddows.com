0e991f07e635d93f552178f77fda1ca2867c24a1
Dan Beddows <dan@cloudmix.net>
2021-07-22 14:20:14 +0100

    added facebook and twitter meta tags, changed description content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -63,18 +63,36 @@ const ContentContainer = styled.div`
 `;
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const aboutUrl = "https://danbeddows.com/";
+  const aboutDescription =
+    "Hi, I'm Dan Beddows, a full stack web developer. I'm available to hire for contract work.";
+
   return (
     <>
       <Head>
         <title>Dan Beddows</title>
-        <meta
-          name="description"
-          content="I'm Dan Beddows, a web developer based in Manchester, UK. I like donuts."
-        />
+
+        <meta name="description" content={aboutDescription} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="UTF-8" />
+
         <link rel="icon" href="/favicon.png" />
+
         <style type="text/css">{fontAwesomeDom.css()}</style>
+
+        <meta property="og:url" content={aboutUrl} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Dan Beddows" />
+        <meta property="og:description" content={aboutDescription} />
+        <meta property="og:title" content="Dan Beddows" />
+        <meta property="og:image" content="" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Dan Beddows" />
+        <meta property="twitter:domain" content="danbeddows.com" />
+        <meta property="twitter:url" content={aboutUrl} />
+        <meta name="twitter:description" content={aboutDescription} />
+        <meta name="twitter:image" content="" />
       </Head>
       <GlobalStyles />
       <FontBasierGlobalStyle />
