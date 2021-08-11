bc9bd0354ef6d660f3d7f7791c3d5a7961f58128
Dan Beddows <dan@cloudmix.net>
2021-08-11 17:15:41 +0100

    split out project component into it's own file

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,14 +1,12 @@
-import { WorkItem } from ".prisma/client";
-import { StackItem, WorkItemStack } from "@prisma/client";
+import { StackItem, WorkItem, WorkItemStack } from "@prisma/client";
 import PageTitle from "components/content/PageTitle";
 import Paragraph from "components/content/Paragraph";
 import Section from "components/content/Section";
-import TechStackIcon from "components/content/TechStackIcon";
 import Title from "components/content/Title";
 import Page from "components/layout/Page";
+import Project from "components/pages/work/Project";
 import prisma from "lib/prisma";
 import { GetStaticProps } from "next";
-import Link from "next/link";
 import styled from "styled-components";
 
 const ProjectsContainer = styled.div`
@@ -23,121 +21,14 @@ const ProjectsContainer = styled.div`
   }
 `;
 
-const ProjectContents = styled.a`
-  width: 100%;
-  height: 400px;
-  border-radius: 12px;
-  position: relative;
-  overflow: hidden;
-  text-decoration: none;
-
-  @media (min-width: ${(props) => props.theme.bp.desktop}) {
-    width: 600px;
-    margin: 30px 15px;
-  }
-`;
-
-const Background = styled.div`
-  width: 100%;
-  height: 320px;
-  background-color: #555;
-  background-size: cover;
-  background-position: center;
-`;
-
-const Foreground = styled.div`
-  width: 100%;
-  height: 80px;
-  background: #2b2b2b;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const ForegroundLeft = styled.div`
-  padding: 6px 18px;
-
-  @media (min-width: ${(props) => props.theme.bp.desktop}) {
-    max-width: 60%;
-  }
-`;
-
-const ProjectTitle = styled.h4`
-  font-size: 20px;
-  font-weight: 500;
-  color: var(--coral);
-  margin: 0;
-`;
-
-const ProjectTeaser = styled.p`
-  font-size: 14px;
-  margin: 0;
-  color: #fff;
-`;
-
-const ForegroundRight = styled.div`
-  padding: 6px 18px;
-  color: #fff;
-  display: block;
-  position: absolute;
-  top: 10px;
-  right: 10px;
-
-  i,
-  svg {
-    font-size: 26px;
-    margin: 0 8px;
-  }
-
-  @media (min-width: ${(props) => props.theme.bp.desktop}) {
-    display: flex;
-    position: relative;
-    max-width: 40%;
-    top: 0;
-    right: 0;
-  }
-`;
-
-type CombinedWorkItem = WorkItem & {
+type WorkItemWithStack = WorkItem & {
   stackItems: (WorkItemStack & {
     stackItem: StackItem;
   })[];
 };
 
-interface ProjectProps {
-  workItem: CombinedWorkItem;
-}
-
-const Project: React.FC<ProjectProps> = ({ workItem }) => {
-  return (
-    <Link href={"/work/" + workItem.slug} passHref>
-      <ProjectContents>
-        <Background
-          style={{ backgroundImage: "url(" + workItem.thumbUrl + ")" }}
-        />
-        <Foreground>
-          <ForegroundLeft>
-            <ProjectTitle>{workItem.title}</ProjectTitle>
-            <ProjectTeaser>{workItem.teaser}</ProjectTeaser>
-          </ForegroundLeft>
-          <ForegroundRight>
-            {workItem.stackItems.map((stackItem, index) => (
-              <TechStackIcon
-                key={index}
-                icon={stackItem.stackItem.icon}
-                title={stackItem.stackItem.name}
-              />
-            ))}
-          </ForegroundRight>
-        </Foreground>
-      </ProjectContents>
-    </Link>
-  );
-};
-
 interface Props {
-  workList: CombinedWorkItem[];
+  workList: WorkItemWithStack[];
 }
 
 const Work: React.FC<Props> = (props) => {
