a8ea3df288518c48f30a814bbc37db26ed4e746a
Dan Beddows <dan@cloudmix.net>
2021-08-13 09:29:16 +0100

    added more comments

--- util/hooks/reactBalloon.tsx
+++ util/hooks/reactBalloon.tsx
@@ -13,6 +13,7 @@ import Paragraph from "components/content/Paragraph";
 import UnorderedList from "components/content/UnorderedList";
 import React, { FunctionComponent, ReactElement, ReactNode } from "react";
 
+// The object structure received after using Reacts React.Children.toArray method
 interface DeflatedComponent {
   props: {
     children: (string | DeflatedComponent)[];
@@ -22,6 +23,25 @@ interface DeflatedComponent {
   };
 }
 
+/**
+ * The custom object structure react-balloon converts to and from JSON
+ *
+ * params:
+ * 	children:
+ * 		if string, the innerHTML of the component
+ * 		if array, the children of the component as an array of InflatedComponents
+ * 	props:
+ * 		all component props except the 'children' prop
+ * 	type:
+ * 		optional value. only set if the component is a HTML element
+ * 		example value: "li"
+ * 	component:
+ * 		optional value. only set if the component is a custom React component
+ * 		example value: "MyCustomComponent"
+ *
+ * 	note: if both 'type' and 'component' is undefined, the component type used
+ * 	will be React.Fragment
+ */
 interface InflatedComponent {
   children: string | InflatedComponent[];
   props: {};
@@ -30,7 +50,7 @@ interface InflatedComponent {
 }
 
 /**
- * declare a string-based list to map to component types when inflating
+ * Declare a string-based list to map to component types when inflating
  */
 const componentLibrary = {
   Paragraph: Paragraph,
@@ -39,13 +59,13 @@ const componentLibrary = {
 };
 
 const deflateRecursive = (element: DeflatedComponent | string) => {
-  // first check whether the element is a string - if so, we're at the end of the node
+  // First check whether the element is a string - if so, we're at the end of the node
   // so we can return and exit
   if (typeof element === "string") {
     return element;
   }
 
-  // get component type, and set a different var based in whether the component
+  // Get component type, and set a different var based in whether the component
   // is a HTML element or React component
   let component, type;
   if (element.type.name) {
@@ -54,19 +74,19 @@ const deflateRecursive = (element: DeflatedComponent | string) => {
     type = element.type;
   }
 
-  // get the child/children of element
+  // Get the child/children of element
   const children = element.props.children as DeflatedComponent[] | string;
 
-  // copy all props other than children
+  // Copy all props other than children
   let props: any = { ...element.props };
   delete props.children;
 
-  // if 'children' is a string, we're at end of the node - return and exit
+  // If 'children' is a string, we're at end of the node - return and exit
   if (typeof children === "string") {
     return { type, component, children, props };
   }
 
-  // if 'children' is an array - map through and deflate those too
+  // If 'children' is an array - map through and deflate those too
   const recursiveChildren: any = children.map((child) =>
     deflateRecursive(child)
   );
@@ -75,13 +95,13 @@ const deflateRecursive = (element: DeflatedComponent | string) => {
 };
 
 const deflate = (e: ReactElement): string => {
-  // turn the element into an array
+  // Turn the element into an array
   const element = React.Children.toArray(e)[0] as DeflatedComponent;
-  console.log(element);
-  // grab the elements data
+
+  // Grab the elements data
   const data = deflateRecursive(element);
 
-  // convert and return JSON
+  // Convert and return JSON
   return JSON.stringify(data);
 };
 
@@ -92,20 +112,22 @@ const createReactComponent = (
   componentType?: string
 ): ReactNode => {
   if (componentType) {
-    // react component
+    // React component
     let component = componentLibrary[componentType];
     return React.createElement(component, props, children);
   }
+
   if (htmlType) {
+    // HTML element
     return React.createElement(htmlType, props, children);
   }
 
-  // neither componentType or htmlType were passed, return a fragment
+  // Neither componentType or htmlType were passed, return a fragment
   return React.createElement(React.Fragment, props, children);
 };
 
 const inflateRecursive = (elem: InflatedComponent, key: number = 0) => {
-  // first check if the children key is a string or an array - if string, we're at the end
+  // First check if the children key is a string or an array - if string, we're at the end
   // of the node and we can exit
   const children = elem.children;
 
@@ -132,7 +154,7 @@ const inflateRecursive = (elem: InflatedComponent, key: number = 0) => {
 };
 
 const inflate = (data: string): ReactNode => {
-  // convert JSON to array
+  // Convert JSON to array
   const arr = JSON.parse(data) as InflatedComponent;
 
   return inflateRecursive(arr);
