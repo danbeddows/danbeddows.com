f01546c36849b4255f0a5489d5a4e39cb1db13a3
Dan Beddows <dan@cloudmix.net>
2023-06-25 18:18:12 +0100

    Edited bio image

--- src/components/global/Springboard/Header/HeaderImage/Image.tsx
+++ src/components/global/Springboard/Header/HeaderImage/Image.tsx
@@ -2,15 +2,29 @@ import Image from "next/image";
 import { FC } from "react";
 import styled from "styled-components";
 
+const ImageContainer = styled.div`
+  max-width: 146px;
+  max-height: 146px;
+`;
+
 const StyledImage = styled(Image)`
   border-radius: 50%;
   overflow: hidden;
-  border: 2px #c6c6c6 solid !important;
-  background: #c6c6c6;
+  border: 2px var(--light-blue) solid !important;
+  background: #fff;
 `;
 
 const HeaderImage: FC = () => {
-  return <StyledImage src={"/danbeddows.jpg"} width="146" height="146" />;
+  return (
+    <ImageContainer>
+      <StyledImage
+        src={"/danbeddows.jpg"}
+        width="292"
+        height="292"
+        quality={100}
+      />
+    </ImageContainer>
+  );
 };
 
 export default HeaderImage;
